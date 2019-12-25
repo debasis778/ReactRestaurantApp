5372aa00c30ccd9474a302ed2553556d674f1c41
Debasis Sutar <debasis778@gmail.com>
2019-12-25 12:19:37 +0530

    host testing

---
+++ precache-manifest.d52001689c20d741aa9bc212c8c11d93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39641f2178c0af654134d5869f6535c",
+    "url": "/ReactRestaurantApp/index.html"
+  },
+  {
+    "revision": "d553919ce458d2406b6a",
+    "url": "/ReactRestaurantApp/static/css/2.89241c46.chunk.css"
+  },
+  {
+    "revision": "5df1a465cd715fc808f7",
+    "url": "/ReactRestaurantApp/static/css/main.645b43b7.chunk.css"
+  },
+  {
+    "revision": "d553919ce458d2406b6a",
+    "url": "/ReactRestaurantApp/static/js/2.64896932.chunk.js"
+  },
+  {
+    "revision": "5df1a465cd715fc808f7",
+    "url": "/ReactRestaurantApp/static/js/main.a9689566.chunk.js"
+  },
+  {
+    "revision": "bbfe436de01d629ade13",
+    "url": "/ReactRestaurantApp/static/js/runtime-main.8822195c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactRestaurantApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactRestaurantApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactRestaurantApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactRestaurantApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactRestaurantApp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
